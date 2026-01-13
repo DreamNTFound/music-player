085e785ae65d3ec13797c1545d4a05f1c2138e9f
DreamNTFound <jenardakatsuki@gmail.com>
2026-01-13 14:24:39 +0800

    Modify its path for both cover & audio

--- src/data/FeaturedTracks.jsx
+++ src/data/FeaturedTracks.jsx
@@ -1,13 +1,14 @@
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { audioMap, coverImage } from "../utils/audioLoader";
 
 export const FeaturedTracks = [
     {
         id: 1,
         title: "Everest",
         artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Everest.mp3",
+        cover: coverImage,
+        audio: audioMap["everest"],
         icon: [faUser, faCalendar],
         yearAdded: "2023"
     },
@@ -16,8 +17,8 @@ export const FeaturedTracks = [
         id: 2,
         title: "Mask",
         artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Mask.mp3",
+        cover: coverImage,
+        audio: audioMap["mask"],
         icon: [faUser, faCalendar],
         yearAdded: "2021"
     },
@@ -26,8 +27,8 @@ export const FeaturedTracks = [
         id: 3,
         title: "Paranoid",
         artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Paranoid.mp3",
+        cover: coverImage,
+        audio: audioMap["paranoid"],
         icon: [faUser, faCalendar],
         yearAdded: "2023"
     },
@@ -36,8 +37,8 @@ export const FeaturedTracks = [
         id: 7,
         title: "Until I End Up Dead",
         artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Until_I_End_Up_Dead.mp3",
+        cover: coverImage,
+        audio: audioMap["until_i_end_up_dead"],
         icon: [faUser, faCalendar],
         yearAdded: "2023"
     },
@@ -46,8 +47,8 @@ export const FeaturedTracks = [
         id: 5,
         title: "Slow Down",
         artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Slow_Down.mp3",
+        cover: coverImage,
+        audio: audioMap["slow_down"],
         icon: [faUser, faCalendar],
         yearAdded: "2023"
     },
@@ -56,8 +57,8 @@ export const FeaturedTracks = [
         id: 6,
         title: "Spotlight",
         artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Spotlight.mp3",
+        cover: coverImage,
+        audio: audioMap["spotlight"],
         icon: [faUser, faCalendar],
         yearAdded: "2023"
     }
--- src/data/QuickPicks.jsx
+++ src/data/QuickPicks.jsx
@@ -1,49 +1,51 @@
+import { audioMap, coverImage } from "../utils/audioLoader";
+
 export const QuickPicks = [
-    {
-        id: 1,
-        title: "Everest",
-        artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Everest.mp3"
-    },
+  {
+    id: 1,
+    title: "Everest",
+    artist: "Dream Music",
+    cover: coverImage,
+    audio: audioMap["everest"],
+  },
 
-    {
-        id: 2,
-        title: "Mask",
-        artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Mask.mp3"
-    },
+  {
+    id: 2,
+    title: "Mask",
+    artist: "Dream Music",
+    cover: coverImage,
+    audio: audioMap["mask"],
+  },
 
-    {
-        id: 3,
-        title: "Paranoid",
-        artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Paranoid.mp3"
-    },
+  {
+    id: 3,
+    title: "Paranoid",
+    artist: "Dream Music",
+    cover: coverImage,
+    audio: audioMap["paranoid"],
+  },
 
-    {
-        id: 4,
-        title: "RoadTrip",
-        artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Roadtrip.mp3"
-    },
+  {
+    id: 4,
+    title: "RoadTrip",
+    artist: "Dream Music",
+    cover: coverImage,
+    audio: audioMap["roadtrip"],
+  },
 
-    {
-        id: 5,
-        title: "Slow Down",
-        artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Slow_Down.mp3"
-    },
+  {
+    id: 5,
+    title: "Slow Down",
+    artist: "Dream Music",
+    cover: coverImage,
+    audio: audioMap["slow_down"],
+  },
 
-    {
-        id: 6,
-        title: "Spotlight",
-        artist: "Dream Music",
-        cover: "/src/assets/images/dream-music.png",
-        audio: "/src/components/song/Spotlight.mp3"
-    }
-]
+  {
+    id: 6,
+    title: "Spotlight",
+    artist: "Dream Music",
+    cover: coverImage,
+    audio: audioMap["spotlight"],
+  },
+];
