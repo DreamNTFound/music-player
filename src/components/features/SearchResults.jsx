8cf9aec529ad70f338326f443bfc943e70e868b6
DreamNTFound <jenardakatsuki@gmail.com>
2026-01-20 15:55:39 +0800

    Minor Changes

--- src/components/features/SearchResults.jsx
+++ src/components/features/SearchResults.jsx
@@ -12,7 +12,7 @@ export default function SearchResults({ query }) {
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 animate-fade-in">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 animate-fade-in">
         {result.map((song) => (
           <div
             key={song.id}
